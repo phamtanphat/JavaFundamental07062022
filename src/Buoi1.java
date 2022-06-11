263858c8c01a4c94b6d4924a1a72a27588adcdd4
Phat <phatdroid94@gmail.com>
2022-06-11 21:13:15 +0700

    2: Declare Variable

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -2,8 +2,18 @@ public class Buoi1 {
     public static void main(String[] args) {
         // 1: Kiểu dữ liệu (Data type)
         // Nhóm 1: Kiểu nguyên thủy
-        // Ví dụ: char (ký tự), int (số nguyên), float (số thực), boolean (kiểu đúng sai)
+        // Ví dụ: char (ký tự), int or long (số nguyên), float or double (số thực), boolean (kiểu đúng sai)
         // Nhóm 2: Kiểu object (Kiểu preference)
         // Ví dụ: Person (Đối tượng tự tạo ra)
+
+        // 2: Khai báo biến
+        char gender = 'M';
+        String name = "Phạm Tấn Phát";
+        int age = 28;
+        float weigh = 75.1f;
+        float height = 1.75f;
+        boolean isBusy = true;
+
+
     }
 }
