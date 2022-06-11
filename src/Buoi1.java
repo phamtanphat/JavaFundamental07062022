a3040b21b85c81638321a6adff83e5f793d60e85
Phat <phatdroid94@gmail.com>
2022-06-11 20:41:58 +0700

    1: Data type

---
+++ src/Buoi1.java
@@ -0,0 +1,9 @@
+public class Buoi1 {
+    public static void main(String[] args) {
+        // 1: Kiểu dữ liệu (Data type)
+        // Nhóm 1: Kiểu nguyên thủy
+        // Ví dụ: char (ký tự), int (số nguyên), float (số thực), boolean (kiểu đúng sai)
+        // Nhóm 2: Kiểu object (Kiểu preference)
+        // Ví dụ: Person (Đối tượng tự tạo ra)
+    }
+}
