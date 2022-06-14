f42f67f1b6215b4672fa703daa573185ccdb4003
Phat <phatdroid94@gmail.com>
2022-06-14 20:46:13 +0700

    4: Toán tử

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -19,5 +19,28 @@ public class Buoi1 {
 //        System.out.println("Giới tính: " + gender);
 //        System.out.println("Tuổi: " + age);
 
+//         4: Toán tử
+//         Toán tử: ++, --
+//         Trường hợp 1: a++
+//              - Lấy giá trị của a và thế vào vị trí a++
+//              - Sau khi gán xong thì sẽ chạy a = a + 1
+//         Trường hợp 2: ++a
+//              - Chạy a = a + 1
+//              - Lấy giá trị của a và thế vào vị trí a++
+        int a = 5;
+        int b = 5;
+
+        int result = ++a - --b + a-- - b-- + a++ - --b - --a;
+        // 6 - --b + a-- - b-- + a++ - --b - --a; a = 6, b = 5
+        // 6 - 4 + a-- - b-- + a++ - --b - --a; a = 6, b = 4
+        // 6 - 4 + 6 - b-- + a++ - --b - --a; a = 5, b = 4
+        // 6 - 4 + 6 - 4 + a++ - --b - --a; a = 5, b = 3
+        // 6 - 4 + 6 - 4 + 5 - --b - --a; a = 6, b = 3
+        // 6 - 4 + 6 - 4 + 5 - 2 - --a; a = 6, b = 2
+        // 6 - 4 + 6 - 4 + 5 - 2 - 5; a = 5, b = 2
+        // result = 2, a = 5, b = 2
+        System.out.println("Result " + result);
+        System.out.println("A " + a);
+        System.out.println("B " + b);
     }
 }
