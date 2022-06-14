7523651de6bd0287caf97ec595742f545ca71b28
Phat <phatdroid94@gmail.com>
2022-06-14 20:19:23 +0700

    3: Method

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -1,19 +1,23 @@
 public class Buoi1 {
     public static void main(String[] args) {
-        // 1: Kiểu dữ liệu (Data type)
-        // Nhóm 1: Kiểu nguyên thủy
-        // Ví dụ: char (ký tự), int or long (số nguyên), float or double (số thực), boolean (kiểu đúng sai)
-        // Nhóm 2: Kiểu object (Kiểu preference)
-        // Ví dụ: Person (Đối tượng tự tạo ra)
-
-        // 2: Khai báo biến
-        char gender = 'M';
-        String name = "Phạm Tấn Phát";
-        int age = 28;
-        float weigh = 75.1f;
-        float height = 1.75f;
-        boolean isBusy = true;
-
+//          1: Kiểu dữ liệu (Data type)
+//         Nhóm 1: Kiểu nguyên thủy
+//         Ví dụ: char (ký tự), int or long (số nguyên), float or double (số thực), boolean (kiểu đúng sai)
+//         Nhóm 2: Kiểu object (Kiểu preference)
+//         Ví dụ: Person (Đối tượng tự tạo ra)
+//
+//         2: Khai báo biến
+//        char gender = 'M';
+//        String name = "Phạm Tấn Phát";
+//        int age = 28;
+//        float weigh = 75.1f;
+//        float height = 1.75f;
+//        boolean isBusy = true;
+//
+//         3: Phương thức (method or function)
+//         Phương thức in trong hệ thống: System
+//        System.out.println("Giới tính: " + gender);
+//        System.out.println("Tuổi: " + age);
 
     }
 }
